831cbd03f8d03e2e8a5fd33e386102d2f5e50b1f
AlfonsoJan <alfonsobusker@gmail.com>
2023-02-15 19:31:08 +0100

    Initial Commit

---
+++ src/main/java/nl/bio/inf/peptidomicswebapp/PeptidomicsWebAppApplication.java
@@ -0,0 +1,13 @@
+package nl.bio.inf.peptidomicswebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeptidomicsWebAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PeptidomicsWebAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nl/bio/inf/peptidomicswebapp/PeptidomicsWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.bio.inf.peptidomicswebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeptidomicsWebAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
