53cca712bc5b7381b7d0b07e5187bbe7d57d16bb
Jan Alfonso <alfonsobusker@gmail.com>
2023-02-18 21:22:17 +0100

    Started the landing page

--- src/main/java/nl/bio/inf/peptidomicswebapp/controller/HomeController.java
+++ src/main/java/nl/bio/inf/peptidomicswebapp/controller/HomeController.java
@@ -1,15 +1,12 @@
 package nl.bio.inf.peptidomicswebapp.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
     @RequestMapping(value = {"", "/", "/home"})
-    public String landingPage(Model model){
-        model.addAttribute("brand", "Peptidomics");
-        model.addAttribute("title", "Peptidomics");
+    public String landingPage(){
         return "index";
     }
 }
