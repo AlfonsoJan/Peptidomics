f87726d39310d823bf35c59d9c317ce782d9e525
Jan Alfonso <alfonsobusker@gmail.com>
2023-02-21 08:51:06 +0100

    Started the upload page

---
+++ src/main/java/nl/bio/inf/peptidomicswebapp/controller/UploadController.java
@@ -0,0 +1,13 @@
+package nl.bio.inf.peptidomicswebapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class UploadController {
+    @RequestMapping(value ="/upload")
+    public String landingPage(){
+        return "upload";
+    }
+}
