4547b63b22a58a06ba8981327d0e519f1c6b5d88
Jan Alfonso <alfonsobusker@gmail.com>
2023-02-28 13:39:56 +0100

    Added footer fragments

--- src/main/resources/static/js/upload.js
+++ src/main/resources/static/js/upload.js
@@ -1,15 +1,34 @@
-
-document.addEventListener('DOMContentLoaded', (event) => {
-    document.querySelectorAll(".switch").forEach((theSwitch) => {
-        theSwitch.addEventListener("click", handleClickEvent, false);
-    });
-    function handleClickEvent(evt) {
-        const el = evt.target;
-
-        if (el.getAttribute("aria-checked") === "true") {
-            el.setAttribute("aria-checked", "false");
-        } else {
-            el.setAttribute("aria-checked", "true");
-        }
+function getInputType(el) {
+    switch(el.value) {
+        case "1":
+            document.getElementById("pdb-input-form").style.display = 'none';
+            document.getElementById("file-upload-form").style.display = '';
+            document.getElementById("input-pdb").required = false;
+            document.getElementById("file-upload").required = true;
+            break;
+        case "2":
+            document.getElementById("pdb-input-form").style.display = '';
+            document.getElementById("file-upload-form").style.display = 'none';
+            document.getElementById("input-pdb").required = true;
+            document.getElementById("file-upload").required = false;
+            break;
+        default:
+            break;
     }
-});
+}
+
+function getFileName(el) {
+    document.getElementById("file-name").textContent = el.files[0].name;
+}
+(function() {
+    document.getElementById("upload-input").checked = true;
+    document.getElementById("pdb-input").checked = false;
+    document.getElementById("pdb-input-form").style.display = 'none';
+    document.getElementById("file-upload").required = true;
+})()
+// document.addEventListener('DOMContentLoaded', (event) => {
+//     (function resetRadioButtons() {
+//         document.getElementById("upload-input").checked = true;
+//         document.getElementById("pdb-input").checked = false;
+//     })()
+// }, false);
