d30fe7a9eb71ec6ab429ad20dba9427a8e1292db
Jan Alfonso <alfonsobusker@gmail.com>
2023-03-06 16:46:18 +0100

    File is saved in the session

--- src/main/java/nl/bio/inf/peptidomicswebapp/controller/UploadController.java
+++ src/main/java/nl/bio/inf/peptidomicswebapp/controller/UploadController.java
@@ -1,40 +1,50 @@
 package nl.bio.inf.peptidomicswebapp.controller;
 
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import nl.bio.inf.peptidomicswebapp.models.UploadedFile;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 
 @Controller
+@SessionAttributes(value = {"uploadedFile"})
 public class UploadController {
     @GetMapping(value ="/upload")
     public String landingPage(){
         return "upload";
     }
 
-    @PostMapping(value = "/result")
+    @PostMapping(value = "/result_from_code")
     public String resultFromCode(@RequestParam("pdb_code") String text) {
         System.out.println(text);
-        return "upload";
+        return "redirect:/result";
     }
     @PostMapping(value = "/result_from_files")
-    public String resultFromFiles(@RequestParam("pdb_file") MultipartFile file) {
-        System.out.println(file);
-        return "upload";
-//        Arrays.asList(files).stream().forEach(file -> System.out.println(file.getOriginalFilename()));
-//        try {
-//            BufferedReader reader = new BufferedReader(new InputStreamReader(file.getInputStream()));
-//            String strCurrentLine;
-//            while ((strCurrentLine = reader.readLine()) != null) {
-//                System.out.println(strCurrentLine);
-//            }
-//        } catch (IOException e) {
-//            throw new RuntimeException(e);
-//        }
+    public String resultFromFiles(HttpSession session,
+                                  @RequestParam("pdb_file") MultipartFile file,
+                                  Model model) throws IOException {
+        model.asMap().put("uploadedFile", new UploadedFile(file.getOriginalFilename(), file.getBytes()));
+        return "redirect:/result";
+    }
+
+    @RequestMapping(value = "/result")
+    public String resultFromFiles(Model model) {
+        try {
+            UploadedFile uploadedFile = (UploadedFile) model.getAttribute("uploadedFile");
+            System.out.println(uploadedFile.originalFilename());
+            return "index";
+        }
+        catch (ClassCastException ex) {
+            return "redirect:/";
+        }
     }
 }
---
+++ src/main/java/nl/bio/inf/peptidomicswebapp/models/UploadedFile.java
@@ -0,0 +1,30 @@
+package nl.bio.inf.peptidomicswebapp.models;
+
+public record UploadedFile(String originalFilename, byte[] data) {
+    @Override
+    public byte[] data() {
+        return data.clone();
+    }
+}
+
+//public class UploadedFile {
+//    private final String originalFilename;
+//    private final byte[] data;
+//
+//    public UploadedFile(String fileName, byte[] data) {
+//        this.originalFilename = fileName;
+//        this.data = data.clone();
+//    }
+//
+//    public String getOriginalFilename() {
+//        return originalFilename;
+//    }
+//
+//    public byte[] getData() {
+//        return data.clone();
+//    }
+//
+//    public int getSize() {
+//        return data.length;
+//    }
+//}
