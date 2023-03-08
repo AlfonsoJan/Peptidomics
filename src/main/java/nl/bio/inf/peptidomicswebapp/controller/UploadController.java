3c499d6dd79e8f834fec7cc2b62fc6519e486808
Alfonso Jan <alfonsobusker@gmail.com>
2023-03-08 15:21:20 +0100

    Changed the functionality of the read in file

--- src/main/java/nl/bio/inf/peptidomicswebapp/controller/UploadController.java
+++ src/main/java/nl/bio/inf/peptidomicswebapp/controller/UploadController.java
@@ -1,23 +1,16 @@
 package nl.bio.inf.peptidomicswebapp.controller;
 
-import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
 import nl.bio.inf.peptidomicswebapp.models.PDB;
-import nl.bio.inf.peptidomicswebapp.models.UploadedFile;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.ModelAndView;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 
 @Controller
-@SessionAttributes(value = {"uploadedFile"})
+@SessionAttributes(value = {"PDBFiles"})
 public class UploadController {
     @GetMapping(value ="/upload")
     public String landingPage(){
@@ -26,26 +19,26 @@ public class UploadController {
 
     @PostMapping(value = "/result_from_code")
     public String resultFromCode(@RequestParam("pdb_code") String pdbCode, Model model) throws IOException {
-        model.asMap().put("uploadedFile", "");
-        PDB pdb = new PDB(pdbCode);
-        String filename = pdb.getStructureId() + ".pdb";
-        model.asMap().put("uploadedFile", new UploadedFile(filename, pdb.getBytes()));
+        model.asMap().put("PDBFiles", new PDB(pdbCode));
         return "redirect:/result";
     }
 
     @PostMapping(value = "/result_from_files")
     public String resultFromFiles(@RequestParam("pdb_file") MultipartFile file,
                                   Model model) throws IOException {
-        model.asMap().put("uploadedFile", new UploadedFile(file.getOriginalFilename(), file.getBytes()));
+        model.asMap().put("PDBFiles", new PDB(file.getOriginalFilename(), file.getBytes(), PDB.getStructureFromInputstream(file.getInputStream())));
         return "redirect:/result";
     }
 
     @RequestMapping(value = "/result")
     public String resultPage(Model model) {
         try {
-            UploadedFile uploadedFile = (UploadedFile) model.getAttribute("uploadedFile");
-            if (uploadedFile != null) {
-                System.out.println(uploadedFile.getBytes());
+            PDB pdbFile = (PDB) model.getAttribute("PDBFiles");
+            if (pdbFile != null) {
+                if (pdbFile.getStructureId() ==  null) {
+                    System.out.println("EMPTY");
+                }
+                model.addAttribute("fileName", "<strong class=\"is-size-2\">Results of: </strong>" + pdbFile.getStructureId());
                 return "results";
             }
             return "redirect:/";
--- src/main/java/nl/bio/inf/peptidomicswebapp/models/PDB.java
+++ src/main/java/nl/bio/inf/peptidomicswebapp/models/PDB.java
@@ -1,27 +1,62 @@
 package nl.bio.inf.peptidomicswebapp.models;
 
+import io.micrometer.common.util.StringUtils;
+
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.Arrays;
+import java.util.Base64;
 
 public class PDB {
     private static final String DOWNLOAD_BY_ID_URL = "https://files.rcsb.org/download/%s.pdb";
 
-    private String structureId;
+    private final String structureId;
+    private final byte[] bytes;
+    private final String fileName;
 
-    public PDB(String structureId) {
+    public PDB(String structureId) throws IOException {
+        this.structureId = structureId;
+        this.bytes = getBytesConnection();
+        this.fileName = structureId + ".pdb";
+    }
+
+    public PDB(String fileName, byte[] fileBytes, String structureId){
+        this.fileName = fileName;
+        this.bytes  = fileBytes;
         this.structureId = structureId;
     }
 
-    public InputStream getInputStream() throws IOException {
+    public static String getStructureFromInputstream(InputStream is) throws IOException {
+        String line;
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(is));
+        while( (line = bufferedReader.readLine()) != null ) {
+            if (line.startsWith("HEADER")) {
+                return line.strip().substring(line.strip().lastIndexOf(" ")+1);
+            }
+        }
+        return "";
+    }
+
+    private InputStream getInputStream() throws IOException {
         URL url = new URL(String.format(DOWNLOAD_BY_ID_URL, this.structureId));
         URLConnection connection = url.openConnection();
         return connection.getInputStream();
     }
 
-    public byte[] getBytes() throws IOException {
+    private byte[] getBytesConnection() throws IOException {
         return getInputStream().readAllBytes();
+    }
+
+    private byte[] getBytes(){
+        return bytes;
+    }
+
+    public String getFileName() {
+        return fileName;
     }
 
     public String getStructureId() {
