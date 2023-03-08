9ea8ab976daa1bae0d3e354013c8bf7888baedc2
Alfonso Jan <alfonsobusker@gmail.com>
2023-03-08 15:55:09 +0100

    Added a js script

---
+++ src/main/resources/static/js/results.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    (function () {
+        let value = document.getElementById("input-pdb").value;
+        console.log(value)
+    })();
+});
