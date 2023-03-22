1840a7eb49460e1b330414ff4101f923cfdc06a4
wouterzeevat <w.h.zeevat@st.hanze.nl>
2023-03-22 15:03:28 +0100

    changed script itself

--- src/main/resources/scripts/plotly_pca_plot.py
+++ src/main/resources/scripts/plotly_pca_plot.py
@@ -4,6 +4,7 @@ import numpy as np
 import sys
 import json
 
+
 class NumpyEncoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, np.ndarray):
@@ -11,16 +12,18 @@ class NumpyEncoder(json.JSONEncoder):
         return json.JSONEncoder.default(self, obj)
 
 
-def get_plot(P):
-    D = ((P[:, :, None, :] - P[:, None, :, :]) ** 2).sum(axis=3).reshape((len(P), -1)) ** 0.5
-    D -= D.mean(axis=0)
-    vals, vecs = np.linalg.eigh(D.T @ (D / len(D)))
-    scores = D @ vecs[:, [-1, -2, -3]]
+def get_plot(p):
+    d = ((p[:, :, None, :] - p[:, None, :, :]) ** 2).sum(axis=3).reshape((len(p), -1)) ** 0.5
+    d -= d.mean(axis=0)
+    vals, vecs = np.linalg.eigh(d.T @ (d / len(d)))
+    scores = d @ vecs[:, [-1, -2, -3]]
     print(json.dumps({'x': scores.T[0], 'y': scores.T[1], 'z': scores.T[2]}, cls=NumpyEncoder))
+
 
 def main(args):
     p = np.load(args[1])
     get_plot(p)
+
 
 if __name__ == "__main__":
     sys.exit(main(sys.argv))
