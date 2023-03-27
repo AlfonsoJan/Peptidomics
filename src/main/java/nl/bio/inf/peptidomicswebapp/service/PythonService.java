03dda48f9eb5c2870012151591df6feb77f52e6c
Alfonso Jan <alfonsobusker@gmail.com>
2023-03-27 15:10:35 +0200

    Fixed the script

--- src/main/java/nl/bio/inf/peptidomicswebapp/service/PythonService.java
+++ src/main/java/nl/bio/inf/peptidomicswebapp/service/PythonService.java
@@ -8,7 +8,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.logging.Logger;
 
-
 @Service
 public class PythonService implements PythonConstructor{
     private static final Logger LOGGER  = Logger.getLogger(PeptidomicsWebAppApplication.class.getName());
