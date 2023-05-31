9df76d6a89dd4a3175b344588444b2dbd9905d78
AlfonsoJan <alfonsobusker@gmail.com>
2023-05-31 15:43:01 +0200

    fixed the test

--- src/test/java/nl/bio/inf/peptidomicswebapp/service/ErrorServiceTest.java
+++ src/test/java/nl/bio/inf/peptidomicswebapp/service/ErrorServiceTest.java
@@ -3,7 +3,10 @@ package nl.bio.inf.peptidomicswebapp.service;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -11,14 +14,16 @@ import static org.junit.jupiter.api.Assertions.*;
  * ErrorService Test class
  * @author Wouter Zeevat
  */
+@SpringBootTest
 class ErrorServiceTest {
 
-    @Disabled
+    @Autowired
+    private ErrorService errorService;
+
     @ParameterizedTest
-    @ValueSource(strings ={"302", "304", "404"})
-    void generateErrorMessage(String parms) {
-        String result = new ErrorService().generateErrorMessage(Integer.parseInt(parms));
-        String message = "There was a code " + parms + " error: ";
-        //assertEquals(result, message);
+    @CsvSource({"302,There was a code 302 error: \"Found\"", "304,There was a code 304 error: Moved Permanently", "404,There was a code 404 error: \"Not Found\". The resource you requested could not be found. Did you type its location correctly?"})
+    void messagetest(String parms, String expected){
+        String result = errorService.generateErrorMessage(Integer.parseInt(parms));
+        assertEquals(result, expected);
     }
 }
