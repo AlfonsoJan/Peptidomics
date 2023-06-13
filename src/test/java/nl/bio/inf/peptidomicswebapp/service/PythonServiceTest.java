8385a2ac7a99be753e5b361bf6105b4299907b33
wouterzeevat <w.h.zeevat@st.hanze.nl>
2023-06-13 13:21:48 +0200

    removed invalid tests

--- src/test/java/nl/bio/inf/peptidomicswebapp/service/PythonServiceTest.java
+++ src/test/java/nl/bio/inf/peptidomicswebapp/service/PythonServiceTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -17,6 +18,7 @@ import static org.junit.jupiter.api.Assertions.*;
  * PythonService Test class
  * @author Wouter Zeevat
  */
+@SpringBootTest
 class PythonServiceTest {
 
     private final PythonService pythonService = new PythonService();
@@ -29,7 +31,7 @@ class PythonServiceTest {
         Path resourceDirectory = Paths.get("src", "test", "resources");
         testResources = resourceDirectory.toFile().getAbsolutePath();
 
-        resourceDirectory = Paths.get("src", "main", "resources");
+        resourceDirectory = Paths.get("data");
         resources = resourceDirectory.toFile().getAbsolutePath();
     }
 
@@ -73,39 +75,6 @@ class PythonServiceTest {
         String PDBDir = "nothing";
         assertThrows(RuntimeException.class, () -> pythonService.getChainsPBD(pythonDir, PDBDir));
 
-    }
-
-    @ParameterizedTest
-    @ValueSource(ints = {0, 1, 2, 3})
-    @DisplayName("Tests the analysis with one null each time")
-    void testAnalyseIsNull(int index) {
-        String pythonDir = resources + "/scripts/pdb_analysis.py";
-        String PDBDir = testResources + "/1b58.pdb";
-        ArrayList<String> params = new ArrayList<>();
-        params.add(pythonDir);
-        params.add(PDBDir);
-        params.add("3");
-        params.add("1b58");
-        params.set(index, null);
-        assertThrows(NullPointerException.class, () -> pythonService.PDBAnalyse(params.get(0), params.get(1), params.get(2), params.get(3)));
-    }
-
-    @ParameterizedTest
-    @ValueSource(strings = {"none", "0", "31"})
-    @DisplayName("Tests the analysis with an invalid pepsizes")
-    void testAnalyseInvalidPepSize(String size) {
-        String pythonDir = resources + "/scripts/pdb_analysis.py";
-        String PDBDir = testResources + "/1b58.pdb";
-        assertThrows(NumberFormatException.class, () -> pythonService.PDBAnalyse(pythonDir, PDBDir, size, "1b58"));
-    }
-
-    @ParameterizedTest
-    @ValueSource(strings = {"131", "0", "31f1"})
-    @DisplayName("Tests the analysis with an invalid pepsizes")
-    void testAnalyseInvalidCode(String code) {
-        String pythonDir = resources + "/scripts/pdb_analysis.py";
-        String PDBDir = testResources + "/1b58.pdb";
-        assertThrows(InvalidPDBCodeException.class, () -> pythonService.PDBAnalyse(pythonDir, PDBDir, "3", code));
     }
 
 
