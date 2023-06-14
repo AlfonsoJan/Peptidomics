98fd3d9b8d30d92b25e22b3aebd284bf822cd2ca
Alfonso Jan <alfonsobusker@gmail.com>
2023-06-14 12:21:49 +0200

    aDDED TEST

--- src/test/java/nl/bio/inf/peptidomicswebapp/controller/ApiControllerTest.java
+++ src/test/java/nl/bio/inf/peptidomicswebapp/controller/ApiControllerTest.java
@@ -5,6 +5,7 @@ import nl.bio.inf.peptidomicswebapp.models.EigenVectors;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -33,6 +34,12 @@ public class ApiControllerTest {
     @DisplayName("Tests the get file name function!")
     void testApiControllerInvalid(int integer) {
         assertThrows(EigenVectorsNotFoundException.class, () -> controller.getEigenVector(integer));
+    }
+
+    @ParameterizedTest
+    @CsvSource({"0.1663496513136032,1", "0.0049478314358955,2", "-0.0003536110912536,3"})
+    void testApiNormal(String expected, String params) {
+        assert(Double.parseDouble(expected)== Double.parseDouble(controller.getEigenVector(Integer.valueOf(params)).getX().get(0)));
     }
 
 }
